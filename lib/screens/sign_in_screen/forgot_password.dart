b3e66f50d3723fbee2316b09ecd9f4e2e58ba9ef
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2023-08-07 14:09:17 +0400

    Update forgot_password.dart

--- lib/screens/sign_in_screen/forgot_password.dart
+++ lib/screens/sign_in_screen/forgot_password.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_sms/flutter_sms.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
